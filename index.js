7535ab33be40d417a679df521f41ff1abeeb3796
Brendan Baird <brendan.b517@gmail.com>
2022-12-20 10:02:59 -0500

    commit tree 5a48c65b09dafa1e5b76105e1f81ed74d08f0c92

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts){
+    for (let i = 0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+
+function writeCards(name, event){
+    const messages = [];
+    for(let i = 0; i < name.length; i++){
+        const message = `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+
+function countDown(int){
+    while (int >= 0){
+        console.log(int);
+        int--;
+    }
+}
